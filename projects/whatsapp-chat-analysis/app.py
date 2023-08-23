48afadf71af7357145a04b45ba16f22ca21df7c4
Vaibhav Singh <87988671+vaibhavsinghmq@users.noreply.github.com>
2023-08-23 21:08:51 +1000

    init commit

---
+++ projects/whatsapp-chat-analysis/app.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 11:02:17 2023
+
+@author: vaibhavsingh
+"""
+
+import streamlit as st
+import preprocessor, util
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+st.config.set_option("server.maxUploadSize", 1)
+
+
+# Main Streamlit app code
+def main():
+    
+    uploaded_file = st.sidebar.file_uploader("Choose a TXT file", accept_multiple_files=False)
+    
+        
+    # Custom CSS for the header
+    header_style = """
+    <style>
+        .fancy-header {
+            background-color: #3498db;
+            color: white;
+            font-size: 36px;
+            font-family: 'Arial', sans-serif;
+            padding: 20px;
+            text-align: center;
+            border-radius: 10px;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+    """
+    
+    # Display the custom CSS using st.markdown
+    st.markdown(header_style, unsafe_allow_html=True)
+    
+    # Display the fancy header
+    st.markdown('<div class="fancy-header">WhatsApp chat analysis</div>', unsafe_allow_html=True)
+    
+    
+    if uploaded_file is not None:
+        
+        st.text(f"File Name: {uploaded_file.name}")
+        st.text(f"File Size: {uploaded_file.size}")
+        st.text(f"File Type: {uploaded_file.type}")
+        
+        
+        bytes_data = uploaded_file.getvalue()
+        
+        data = bytes_data.decode("utf-8")
+        
+        wa_users_data_df = preprocessor.preprocess(data) # create columns from chat file
+        ##st.text(wa_users_data_df)
+        
+        # Create a dropdown for year
+        year_option = wa_users_data_df['year'].unique().tolist()
+        year_option.sort()
+        year_option.insert(0,"Overall")
+        
+        ## Create a dropdown for users
+        user_option = wa_users_data_df['user'].unique().tolist()
+        user_option.sort()
+        user_option.insert(0,"Overall")
+
+        selected_year = st.sidebar.selectbox("Select an year:", year_option) #selected year
+        selected_user = st.sidebar.selectbox("Show analysis of ",user_option)  # Selected user
+        
+        if st.sidebar.button("Show Analysis"):
+            
+            custom_styles = """
+            <style>
+                .appview-container .block-container{
+                    max-width: 100%;    
+                }
+                .custom-div {
+                    padding: 10px;
+                    border: 1px solid #ccc;
+                    background-color: #fff;
+                    text-align: center;
+                    margin: 5px;
+                }
+                .custom-container {
+                    display: flex;
+                    justify-content: space-between;
+                    margin: -5px; /* Compensate for margin added to divs */
+                }
+            </style>
+            """
+            
+            # Display the custom styles using st.markdown
+            st.markdown(custom_styles, unsafe_allow_html=True)
+            
+
+            # Select the dataframe
+            wa_users_data_df = util.get_selected_dataframe(selected_year,selected_user,wa_users_data_df)
+            
+            
+            # Check if the DataFrame is empty
+            if not wa_users_data_df.empty:
+                
+                
+                # Stats Area
+                num_messages, words, num_media_messages, num_links = util.fetch_stats(wa_users_data_df)
+    
+                with st.container():
+                    col1, col2, col3, col4 = st.columns(4)
+                    
+                    col1.markdown('<div class="custom-div"><div>Total Messages</div><div>{}</div></div>'.format(num_messages), unsafe_allow_html=True)
+                    col2.markdown('<div class="custom-div"><div>Total Words</div><div>{}</div></div>'.format(words), unsafe_allow_html=True)
+                    col3.markdown('<div class="custom-div"><div>Media Shared</div><div>{}</div></div>'.format(num_media_messages), unsafe_allow_html=True)
+                    col4.markdown('<div class="custom-div"><div>Links Shared</div><div>{}</div></div>'.format(num_links), unsafe_allow_html=True)
+    
+                # Create vertical space between containers
+                st.markdown("<br><br>", unsafe_allow_html=True)
+    
+                with st.container():
+                    col1, col2, col3, col4 = st.columns(4)
+                
+    
+                    with col1:
+                        # daily activity
+                        fig, ax = plt.subplots()
+                        util.hourly_activity(wa_users_data_df).plot(kind='bar')
+                        plt.title('Message Frequency by Hour:')
+                        ax.set_xlabel('Hours')
+                        st.pyplot(fig)
+                        
+                    with col2:
+                        # weekly activity
+                        fig, ax = plt.subplots()
+                        util.weekly_activity(wa_users_data_df).plot(kind='bar')
+                        plt.title('Message Frequency by Day of the Week:')
+                        plt.xticks(range(7), ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
+                        ax.set_xlabel('Day of week')
+                        st.pyplot(fig)
+                        
+                    with col3:
+                        # Create the bar plot
+                        # Custom month labels
+                        month_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
+                        
+                        fig, ax = plt.subplots()
+                        monthly_counts =util.monthly_activity(wa_users_data_df)
+                        monthly_counts.plot(kind='bar')
+                        plt.title('Message Frequency by Month:')
+                        plt.xlabel('Month')
+                        plt.xticks(monthly_counts.index - 1, [month_labels[i - 1] for i in monthly_counts.index])  # Set custom month labels
+                        st.pyplot(fig)
+                        
+                           
+                        
+                    with col4:
+                        # emoji analysis
+                        emoji_df = util.emoji_helper(wa_users_data_df)
+                        st.dataframe(emoji_df, height=270)
+    
+                    st.markdown("<br>", unsafe_allow_html=True)
+    
+                    col1, col2= st.columns(2)
+                    
+                    with col1:
+                        # daily activity
+                        fig, ax = plt.subplots()
+                        plt.title('Most common words')
+                        plt.imshow(util.keyword_analysis(wa_users_data_df), interpolation='bilinear')
+                        plt.axis('off')
+                        st.pyplot(fig)
+    
+                    with col2:
+                       
+                        fig, ax = plt.subplots(figsize=(20, 16))
+                        
+                        # Call the sentiment_helper function and plot the data
+                        util.sentiment_helper(wa_users_data_df).plot(marker='o', ax=ax)
+                        
+                        # Customize the plot
+                        ax.set_title('Average Sentiment Scores Over Months', fontsize=20)
+                        ax.set_xlabel('Month')
+                        ax.set_ylabel('Average Sentiment Score', fontsize=15)
+                        ax.set_ylim(-1, 1)  # Set y-axis limits for sentiment score
+                        ax.grid()
+                        
+                        # Display the plot using st.pyplot()
+                        st.pyplot(fig)
+                        
+                    if selected_user == 'Overall':
+                        
+                        st.markdown("<br><br>", unsafe_allow_html=True)
+                        
+                        # Create a figure and axes
+                        fig, ax = plt.subplots(figsize=(20, 16))
+                        
+                        df_plot = wa_users_data_df.copy()
+                        G = util.network_analysis(df_plot)
+                        
+                        # Create the network graph plot
+                        pos = nx.spring_layout(G)
+                        nx.draw_networkx_nodes(G, pos, node_size=500)
+                        nx.draw_networkx_edges(G, pos, width=0.5, alpha=0.5)
+                        nx.draw_networkx_labels(G, pos, font_size=10, font_color='black')
+                        plt.title('Network Graph of User Interactions')
+                        plt.axis('off')  # Turn off axis
+                        st.pyplot(fig)  # Display the network graph plot
+                        
+                        # Calculate centrality measures to identify influential users
+                        in_degree_centrality = nx.in_degree_centrality(G)
+                        out_degree_centrality = nx.out_degree_centrality(G)
+                        
+                        
+                        # Get the first value (occurrence count)
+                        first_occurrence = next(iter(in_degree_centrality.values()))
+                        
+                        
+                        # Print the most influential users based on in-degree centrality
+                        st.text("Top Influential Users based on In-Degree Centrality:")
+                        for user, centrality in sorted(in_degree_centrality.items(), key=lambda x: x[1], reverse=True)[:6]:
+                            st.text(f"{user}: {centrality:.4f}")
+                        
+                        # Print the most influential users based on out-degree centrality
+                        out_sec = 0
+                        st.text("\nTop Influential Users based on Out-Degree Centrality:")
+                        for user, centrality in sorted(out_degree_centrality.items(), key=lambda x: x[1], reverse=True)[:6]:
+                            if out_sec == 0:
+                                out_sec = centrality
+                            st.text(f"{user}: {centrality:.4f}")
+                          
+                            
+                       
+                        st.markdown(
+                            "In this group of friends who like to talk to each other. Some friends talk more, and some talk less. These numbers show us which friends are really good at talking and who others like to talk to the most. "
+                            "Top Influential Users based on In-Degree Centrality: These are the friends who get talked to a lot. "
+                            "For example, the first number {} means that one friend got talked to by {}% of the other friends. "
+                            "It's like saying approx {} out of every 10 friends like to talk to this friend a lot. "
+                            "Top Influential Users based on Out-Degree Centrality: These are the friends who like to start conversations a lot. "
+                            "For example, the first number {} means that this friend likes to start conversations with {}% of the other friends. "
+                            "So, these numbers help us know which friends are really popular in the group and who likes to talk the most to others. "
+                            "Just like in school, where some kids are really good at making friends and starting conversations."
+                            "".format(
+                                first_occurrence, (first_occurrence * 100), (int(first_occurrence * 10)),
+                                out_sec, (out_sec * 100)
+                            )
+                        )
+            else:
+                st.text("Sorry! no data is available. Try another filter")
+                        
+
+if __name__ == "__main__":
+    main()
---
+++ projects/whatsapp-chat-analysis/preprocessor.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 11:10:11 2023
+
+@author: vaibhavsingh
+"""
+
+import re,os
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+
+
+
+
+
+pattern2=r'\[(.*?)\] (.*?): (.*?)$'
+
+## ANdroid code
+def identify_and_convert_date(date_string):
+    possible_formats = [
+        "%m/%d/%y, %I:%M %p",
+        "%d/%m/%Y, %I:%M %p"
+    ]
+
+    for format_str in possible_formats:
+        try:
+            parsed_date = datetime.strptime(re.split(r' - |: ', date_string)[0], format_str)
+            formatted_date = parsed_date.strftime('%-m/%-d/%y, %-I:%M %p')
+            return date_string.replace(re.split(r' - |: ', date_string)[0], formatted_date, 1)
+        except ValueError:
+            pass
+    
+    return date_string
+
+def make_common_date_format(attr_keys,lines):
+  common_date_lines=[]
+  for date_string in lines:
+
+      converted_date = identify_and_convert_date(date_string)
+      common_date_lines.append(converted_date)
+  st.text(f"==STAGE 1== {attr_keys}::=> Original lines {len(lines)}, common date lines {len(common_date_lines)}")
+  return common_date_lines
+
+
+def filter_line(line):
+    return "joined using this group's invite link" not in line and "changed to" not in line and "Messages and calls are end-to-end encrypted." not in line
+
+def is_valid_line(line):
+    return '-' in line.split(" - ")[1] if "changed to" in line else True
+
+def convert_to_24_hour_format(match):
+    return datetime.strptime(match.group(), '%m/%d/%y, %I:%M %p').strftime('[%d/%m/%Y, %H:%M:%S]')
+
+def remove_non_compliant_lines(attr_keys,lines):
+    check_non_compliant_pattern = r'\[(\d{1,2}\/\d{1,2}\/\d{4}, \d{2}:\d{2}:\d{2})\] (.*?): (.*)$'
+
+    non_compliant_lines = []
+
+    for line in lines:
+        if not re.match(check_non_compliant_pattern, line):
+            non_compliant_lines.append(line)
+
+    total_lines = len(lines)
+    non_compliant_count = len(non_compliant_lines)
+    percentage_of_non_compliant_lines = (non_compliant_count / total_lines) * 100
+    st.text(f"==STAGE 3== {attr_keys}::=> Total lines: {total_lines}, Non-compliant lines: {non_compliant_count},Percentage of non-compliant lines: {percentage_of_non_compliant_lines:.2f}%")
+
+    if percentage_of_non_compliant_lines < 10:
+        st.text("Removing non-compliant lines due to high percentage.")
+        lines = [line for line in lines if re.match(check_non_compliant_pattern, line)]
+        st.text(f"Return Compliant lines: {len(lines)}")
+        return lines
+    else:
+        for line in non_compliant_lines:
+            print(line.strip())  # Print the non-compliant lines without leading/trailing whitespace
+
+        st.text(f"==STAGE 3-ERROR== {attr_keys}::=> Return Empty Array as Non-Compliant lines >10%:===> {len(non_compliant_lines)}")
+        return []
+
+
+def process_lines(attr_keys,lines):
+    filtered_lines = [line for line in lines if filter_line(line) and is_valid_line(line)]
+    st.text(f"==STAGE 2A== {attr_keys}::=> Original lines {len(lines)}, filtered_lines {len(filtered_lines)}")
+
+    pattern = r'\d{1,2}/\d{1,2}/\d{2}, \d{1,2}:\d{2}(?:\s*[APapMm]{2})'
+    datetime_lines = [re.sub(pattern, convert_to_24_hour_format, line) for line in filtered_lines]
+    st.text(f"==STAGE 2B== {attr_keys}::=> Original lines {len(lines)}, datetime_lines {len(datetime_lines)}")
+
+    merged_lines = []
+    current_line = ''
+
+    for line in datetime_lines:
+        if line.startswith('[') and current_line:
+            merged_lines.append(current_line.strip())
+            current_line = line.strip()
+        else:
+            current_line += ' ' + line.strip()
+
+    if current_line:
+        merged_lines.append(current_line.strip())
+
+    st.text(f"==STAGE 2C== {attr_keys}::=> Original lines {len(lines)}, merged_lines {len(merged_lines)}")
+
+    output_lines = []
+
+    for line in merged_lines:
+        index = line.find(']')
+        if index != -1:
+            hyphen_index = line.find('- ', index)
+            if hyphen_index != -1:
+                line = line[:hyphen_index] + line[hyphen_index+2:]
+        output_lines.append(line)
+    
+    ##st.text(f"==STAGE 2D== {attr_keys}::=> Original lines {len(lines)}, merged_lines {len(output_lines)}")
+
+    return output_lines
+
+
+
+def has_special_characters(line):
+    for char in line:
+        if ord(char) < 32 or ord(char) > 127:
+            return True
+    return False
+
+
+def extract_data(matches):
+    # Create lists for each column
+    chat_timestamps = [match[0] for match in matches]
+    users = [match[1] for match in matches]
+    messages = [match[2] for match in matches]
+    
+    return chat_timestamps, users, messages
+
+
+## Take a text file as in input and return a dataframe
+def preprocess(data):
+   try:
+    
+        content_without_u200e = data.replace('\u200E', '')
+
+        attr_keys={"name:":"1sqdwe.txt","Size":1234,"Mode":5678}
+
+
+        lines = content_without_u200e.split("\n")
+        ##st.text(lines)
+        
+        common_date_lines = make_common_date_format(attr_keys,lines)
+        output_lines = process_lines(attr_keys,common_date_lines)
+        output_lines = remove_non_compliant_lines(attr_keys,output_lines)
+
+        ##st.text(f"pattern2::{pattern2}")
+        ##st.text(f"output_lines::{output_lines}")
+        ##st.text(f"output_lines::{len(output_lines)}, type:==>{type(output_lines)}")
+        
+        datetime_list = []
+        user_list = []
+        message_list = []
+        
+        for entry in output_lines:
+            match = re.match(pattern2, entry)
+            if match:
+                datetime_list.append(match.group(1))
+                user_list.append(match.group(2))
+                message_list.append(match.group(3))
+        
+        
+        # Create a Pandas DataFrame
+        columns = ['chat_timestamp', 'user', 'message']
+        df = pd.DataFrame(list(zip(datetime_list, user_list, message_list)), columns=columns)
+
+        # Convert 'chat_timestamp' to datetime
+        df['chat_timestamp'] = pd.to_datetime(df['chat_timestamp'], dayfirst=True)
+
+        df['only_date'] = df['chat_timestamp'].dt.date
+        df['year'] = df['chat_timestamp'].dt.year
+        df['month_num'] = df['chat_timestamp'].dt.month
+        df['month'] = df['chat_timestamp'].dt.month_name()
+        df['day'] = df['chat_timestamp'].dt.day
+        df['day_of_week'] = df['chat_timestamp'].dt.day_name()
+        df['hour'] = df['chat_timestamp'].dt.hour
+        df['minute'] = df['chat_timestamp'].dt.minute
+    
+        
+    
+   except Exception as e:
+        st.error("An error occurred:")
+        st.error(str(e))
+        print("Something went wrong",e)
+        df = pd.DataFrame({})
+        
+    
+   return df
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
